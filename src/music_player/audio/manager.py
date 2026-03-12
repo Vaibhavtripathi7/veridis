4d9156ca56a7ebe310cef107d33d8f3bcad4836b
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-03-12 23:29:38 +0530

    Fix: import error

--- src/music_player/audio/manager.py
+++ src/music_player/audio/manager.py
@@ -1,5 +1,5 @@
-from src.music_player.audio.audio import AudioEngine
-from src.music_player.library.musicfileman import MusicFiles
+from .audio import AudioEngine
+from ..library.musicfileman import MusicFiles
 import threading
 import time
 from typing import Optional
