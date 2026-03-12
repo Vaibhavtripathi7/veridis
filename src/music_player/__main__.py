4c682ad1a6773b42273ca9fc775cf8a6c1c79615
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-03-13 00:07:51 +0530

    fix: add a error handling block

--- src/music_player/__main__.py
+++ src/music_player/__main__.py
@@ -1,8 +1,13 @@
 from .interface.tui import AudioPlayer
-
+import time 
 def main():
-    app = AudioPlayer()
-    app.run()
+    time.sleep(0.1)
+    try:
+        app = AudioPlayer()
+        app.run()
+    except Exception as e:
+        print(f"{e}", file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
